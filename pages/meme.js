b88c8e8fbba6366ef28e42d8e3358099b3c92e29
Anika Rede <arede22@berkeley.edu>
2019-09-05 03:14:11 -0700

    added gifs

--- pages/meme.js
+++ pages/meme.js
@@ -1,5 +1,9 @@
 function Meme() {
-  return <img src="/static/spongebob-me-me.jpeg" hspace="20px" alt="SpongieBob" />
+  return <div> 
+		  	<img src="/static/coming-soon.gif" width="300px" height="300px" hspace="20px" alt="Coming Soon" />
+		  	<img src="/static/spongebob-me-me.jpeg" width="300px" height="500px" hspace="20px" alt="SpongeBob" />
+		  	<img src="/static/oskiii.gif" width="300px" height="300px" hspace="20px" alt="Oskiii" />
+  		</div>
 }
 
 export default Meme
