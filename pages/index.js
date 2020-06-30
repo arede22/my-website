070cdd258f5494e4b8920d7e8480fc8527ea9809
Anika Rede <arede22@berkeley.edu>
2020-06-29 22:18:52 -0400

    created global style

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { createGlobalStyle } from 'styled-components';
 import { Header, Nav, Welcome, Intro, Portfolio, ContactMe, Footer } from '../src/components';
 import { theme } from '../src/styles';
-const { colors, fonts, fontSizes, displays } = theme;
+const { colors, fonts } = theme;
 // flexbox: calhacks.org/flexbox
 // position: relative; --> to make B move relative to A and C divs
 // material design, bootstrap, css tricks, mdn web docs
 
 // styles and wrappers
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    font-family: ${fonts.Lucida};
+    background-color: ${colors.bodyBackground};
+  }
+`;
 const StyledWrapper = styled.div`
   background: ${colors.darkGray} url('../static/starting-background.png') no-repeat center;
   background-size: 100% 100%;
@@ -17,30 +24,21 @@ const StyledWrapper = styled.div`
 // export main component
 export default function Home() {
   return (
-    <StyledWrapper>
-      <Header />
+    <React.Fragment>
+      <GlobalStyle />
+      <StyledWrapper>
+        <Header />
 
-      <Nav />
+        <Nav />
 
-      <Welcome />
-      <Intro />
-      <Portfolio />
-      <ContactMe />
+        <Welcome />
+        <Intro />
+        <Portfolio />
+        <ContactMe />
 
-      <Footer />
+        <Footer />
 
-      <style jsx>{`
-        :global(body) {
-          margin: 0;
-          font-family: ${fonts.Lucida};
-          background-color: ${colors.bodyBackground};
-          padding-top: 60px;
-        }
-        .nav-up {
-          top: -66px;
-        }
-      `}</style>
-
-    </StyledWrapper>
+      </StyledWrapper>
+    </React.Fragment>
   )
 };
