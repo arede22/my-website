d8cbde4fe760a46ad9cbf3d2fd6564950fd73b7d
Anika Rede <arede22@berkeley.edu>
2020-07-18 12:45:35 -0400

    trying merge

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import Head from 'next/head';
 
+// styles and wrappers
+const StyledWrapper = styled.div`
+  margin: 0;
+`;
+
 // export main component
 export default function Header() {
   return (
