55605620f9b36ad4b34a490e44cf8ea16a0dac16
Anika Rede <arede22@berkeley.edu>
2020-07-18 12:40:14 -0400

    mistakes from merge

--- src/components/header.js
+++ src/components/header.js
@@ -6,21 +6,25 @@ const StyledWrapper = styled.div`
   margin: 0;
 `;
 
-// main component
-const Header = () => (
+// export main component
+export default function Header() {
+  return (
     <Head>
       <link rel="apple-touch-icon" sizes="180x180" href="../../static/favicon/apple-touch-icon.png"/>
       <link rel="icon" type="image/png" sizes="32x32" href="../../static/favicon/favicon-32x32.png"/>
       <link rel="icon" type="image/png" sizes="16x16" href="../../static/favicon/favicon-16x16.png"/>
       <link rel="manifest" href="../../static/favicon/site.webmanifest"/>
 
+      <link href="https://fonts.googleapis.com/css?family=Oswald" rel="stylesheet" />
+
       <title>Anika Rede: Software Engineer</title>
 
-      <script async src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js" />
-      <script async src="../../static/nav-up.js" />
-      <script async src="../../static/modal.js" />
+      <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript" />
+      <script src="../../static/scripts/nav-up.js" type="text/javascript" />
+      <script src="../../static/scripts/anchor-assignment.js" type="text/javascript" />
+      <script src="../../static/scripts/mobile-home-hover.js" type="text/javascript" />
+      <script src="../../static/scripts/modal-loader.js" type="text/javascript" />
+      <script src="../../static/scripts/scroll-top.js" type="text/javascript" />
     </Head>
-);
-
-// export
-export default Header;
+  )
+};
